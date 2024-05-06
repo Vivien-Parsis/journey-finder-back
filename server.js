669eee9713c9810c04350d0a7909f764a95980e8
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-05-06 10:43:11 +0300

    changed route

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const fastify = require('fastify')({logger: true})
-const cors = require("@fastify/cors")
 const dotenv = require("dotenv")
-const user = require("./src/router/user")
 const { host, port } = require("./src/const/config")
 const { userlist } = require("./src/controller/db")
 
 dotenv.config()
 
-fastify.register(cors, { 
+fastify.register(require("@fastify/cors"), { 
     origin:"*"
 })
 
 fastify.get("/", (req, res) => {
     res.send("Journey Finder Back")
 })
-fastify.register(user, { prefix : "/user" })
+fastify.register(require("./src/router/user"), { prefix : "/user" })
 
 fastify.listen({host: host, port: port }, (err, address) => {
     if (err) throw err
--- src/router/user.js
+++ src/router/user.js
@@ -2,5 +2,21 @@ module.exports = (fastify, _, done) => {
     fastify.get("/",(req, res)=>{
         res.send("hello")
     })
+    fastify.post("/signup",async (req, res)=>{
+        const body = req.body
+        res.send("/signup\n"+body)
+    })
+    fastify.post("/signin",async (req, res)=>{
+        const body = req.body
+        res.send("/signin\n"+body)
+    })
+    fastify.post("/signout",async (req, res)=>{
+        const body = req.body
+        res.send("/signout\n"+body)
+    })
+    fastify.post("/forgetpswd",async (req, res)=>{
+        const body = req.body
+        res.send("/forgetpswd\n"+body)
+    })
     done()
 } 
