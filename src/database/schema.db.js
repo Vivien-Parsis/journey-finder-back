61e1f7e27096cfc2469eb77c56d2ed8edb5bfba3
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-05-29 22:27:37 +0300

    update again schema

--- src/database/schema.db.js
+++ src/database/schema.db.js
@@ -3,20 +3,20 @@ const { mongoose } = require("mongoose")
 const clientSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     firstName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    about : {
-        myCountry:{ type: String, default: "France"},
-        myBudget:{ type: String, default: "Normal"},
-        visitedCountries:{ type: String, default: "Italy"},
-        transportationMod:{ type: String, default: "Train"},
+    email: { type: String, required: true, match:/[a-zA-Z0-9._\-]{1,30}[@][a-zA-Z0-9._\-]{4,12}[.]{1}[a-zA-Z]{2,4}/gm },
+    password: { type: String, required: true},
+    about: {
+        myCountry: { type: String, default: "France" },
+        myBudget: { type: String, default: "Normal" },
+        visitedCountries: { type: String, default: "Italy" },
+        transportationMod: { type: String, default: "Train" },
     }
-})
+}, { versionKey: false })
 
 const tripClientSchema = new mongoose.Schema({
-    client : { type : mongoose.Schema.Types.ObjectId, require : true},
-    trip : { type : Object, require : true },
-    createdDate : { type : Date, default: Date }
-})
+    client: { type : mongoose.Schema.Types.ObjectId, require : true },
+    trip: { type : Object, require : true },
+    createdDate: { type : Date, default: Date }
+}, { versionKey: false })
 
 module.exports = { clientSchema, tripClientSchema }
