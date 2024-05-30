a99008f3d41dc99df9f0cc57de7836c6d076d049
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-05-30 11:23:12 +0300

    add new config const

--- server.js
+++ server.js
@@ -1,32 +1,11 @@
 const dotenv = require("dotenv")
-const { host, port } = require("./src/constant/config.const")
+const { host, port, logger, rateLimit, cors } = require("./src/constant/config.const")
 
-const fastify = require('fastify')({
-    logger: {
-        transport: {
-            target: "@fastify/one-line-logger", 
-            colors: {
-                31: "yellow",
-                32: "magenta"
-            },
-            options: {
-                colorize: true,
-            }
-        },
-        customLevels: {
-            db: 31,
-            ai: 32,
-        },
-    }})
+const fastify = require('fastify')(logger)
 
 dotenv.config()
-fastify.register(require("@fastify/cors"), { 
-    origin:"*"
-})
-fastify.register(require("@fastify/rate-limit"), {
-    max: 10,
-    timeWindow: 60000
-})
+fastify.register(require("@fastify/cors"), cors)
+fastify.register(require("@fastify/rate-limit"), rateLimit)
 
 fastify.get("/", (req, res) => {
     res.send("Journey Finder Back")
--- src/constant/config.const.js
+++ src/constant/config.const.js
@@ -2,13 +2,46 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const db_url = process.env.DB_URL ? process.env.DB_URL : ""
+
 const host = ("RENDER" in process.env || process.env.ISDOCKER=="true") ? `0.0.0.0` : `localhost`
+
 const port = process.env.PORT ? process.env.PORT : 4000
+
 const apiKeyTogether = process.env.API_KEY_TOGETHER ? process.env.API_KEY_TOGETHER : ""
+
+const logger = {
+    logger: {
+        transport: {
+            target: "@fastify/one-line-logger", 
+            colors: {
+                31: "yellow",
+                32: "magenta"
+            },
+            options: {
+                colorize: true,
+            }
+        },
+        customLevels: {
+            db: 31,
+            ai: 32,
+        },
+    }
+}
+
+const cors = { 
+    origin:"*"
+}
+
+const rateLimit = {
+
+}
 
 module.exports = {
     host,
     port,
+    cors,
     db_url,
-    apiKeyTogether
+    logger,
+    rateLimit,
+    apiKeyTogether,
 }
